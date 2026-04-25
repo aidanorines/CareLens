a06448610896aef1f558a91fb7cec61ee645c702
SabidMahmud <sabidmahmud01@gmail.com>
2026-04-25 11:58:18 -0500

    add json and cors middleware

--- projects/Bobcat Surge/server.js
+++ projects/Bobcat Surge/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.json({
+    message: "CareLens backend is running",
+    status: "OK"
+  });
 });
 
 app.listen(PORT, () => {
